e65f19f6e0927b1b1dedf57ac3811c17390daed3
unknown <rone.caballero@gmail.com>
2019-03-31 21:20:29 -0700

    Added door object with rotation script.

--- Section03/BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
+++ Section03/BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeBuildingEscape_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/BuildingEscape",
 				PKG_CompiledIn | 0x00000000,
-				0x4854A74E,
-				0x890069A0,
+				0x2F4E6B5C,
+				0xEEE69D44,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
